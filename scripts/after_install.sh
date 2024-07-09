8415d547e54f3aee2d8a95e36b90c81d64e0d76e
Sumit Grepix <sumit.g@grepix.in>
2024-07-09 17:18:14 +0530

    changes new

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,6 +5,10 @@ cd /app
 
 # install dependencies
 npm install
-npm run build
-cp -r build/* /var/www/html
+
+# install create-react-app and react-scripts
+# without react-scripts application cannot be started
+npm install --save create-react-app react-scripts
+
+# install pm2 process manager
 npm install pm2 -g
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,9 +4,5 @@
 cd /app
 
 # install node and npm
-apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install nodejs -y
-apt-get install npm -y
-apt install nginx -y
-ufw allow 'Nginx HTTP'
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
+yum -y install nodejs npm
